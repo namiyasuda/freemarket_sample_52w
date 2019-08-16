93b70a53a77c05cabd0cef7c4fff01d2b12825c7
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-08-17 00:32:16 +0900

    create ログインページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tops, only: [:index]
-  resources :users, only: [:new]
+  resources :users, only: [:new,:index]
 end
