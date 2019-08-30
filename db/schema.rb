1725d6ec51f6978b7d4603b460be917b597640fa
Guchi-Y <i25mas25@gmail.com>
2019-08-30 19:50:30 +0900

    categoriesテーブルのancestryカラムにindexを設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_051148) do
+ActiveRecord::Schema.define(version: 2019_08_30_033525) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
 end
