fa179abe90f2c6c9dea971783292f98eb03defa3
YusukeHoshin0 <h1133082@gmail.com>
2019-09-05 18:53:50 +0900

    form_withに変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def new
     @category_parents = Category.where(ancestry: nil)
+    @product = Product.new
   end
 
    # 親カテゴリーが選択された後に動くアクションAjax
@@ -14,4 +15,28 @@ class ProductsController < ApplicationController
     @category_grandchildren = Category.find(params[:child_id]).children
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to new_product_path
+    else
+      @category_parents = Category.where(ancestry: nil)
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name, 
+      :description, 
+      :category_id, 
+      :size_id, 
+      :state_id, 
+      :paying_side_id, 
+      :prefecture_id, 
+      :delivery_day_id, 
+      :price)
+  end
 end
