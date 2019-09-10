8a3fb7f6c3ba316eecbf776becbca53618d40468
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-09-10 12:19:53 +0900

    signup.js,haml 一部name属性の修正

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,12 +1,12 @@
   // パスワード確認フォーム用
-function CheckPassword(confirm){
+function CheckPassword(password_confirmation){
   // 入力値取得
   var input1 = password.value;
-  var input2 = confirm.value;
+  var input2 = password_confirmation.value;
   // パスワード比較
   if(input1 != input2){
-    confirm.setCustomValidity("入力値が一致しません。");
+    password_confirmation.setCustomValidity("入力値が一致しません。");
   }else{
-    confirm.setCustomValidity('');
+    password_confirmation.setCustomValidity('');
   }
 }
