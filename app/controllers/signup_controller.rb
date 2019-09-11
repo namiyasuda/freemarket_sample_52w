26969ea3f0648bedfcb4d5a6bc8667eb48087991
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-09-11 18:02:18 +0900

    Slider検出箇所修正

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,79 +1,79 @@
 class SignupController < ApplicationController
  
-  def index
-  end
+def index
+end
 
-  def new  
-  end
+def new  
+end
 
-  def customer_info 
-    @user = User.new
-  end
+def customer_info 
+  @user = User.new
+end
 
-  def sms_comfi
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
-    session[:last_name] = user_params[:last_name]
-    session[:first_name] = user_params[:first_name]
-    session[:last_name_kana] = user_params[:last_name_kana]
-    session[:first_name_kana] = user_params[:first_name_kana]
-    session[:birth_year] = user_params[:birth_year]
-    session[:birth_month] = user_params[:birth_month]
-    session[:birth_day] = user_params[:birth_day]
-    @user = User.new 
-  end
-  def address
-  end
+def sms_comfi
+  session[:nickname] = user_params[:nickname]
+  session[:email] = user_params[:email]
+  session[:password] = user_params[:password]
+  session[:password_confirmation] = user_params[:password_confirmation]
+  session[:last_name] = user_params[:last_name]
+  session[:first_name] = user_params[:first_name]
+  session[:last_name_kana] = user_params[:last_name_kana]
+  session[:first_name_kana] = user_params[:first_name_kana]
+  session[:birth_year] = user_params[:birth_year]
+  session[:birth_month] = user_params[:birth_month]
+  session[:birth_day] = user_params[:birth_day]
+  @user = User.new 
+end
 
-  def create
-    @user = User.new(
-      nickname: session[:nickname],
-      email: session[:email],
-      password: session[:password],
-      password_confirmation: session[:password_confirmation],
-      last_name: session[:last_name], 
-      first_name: session[:first_name], 
-      last_name_kana: session[:last_name_kana], 
-      first_name_kana: session[:first_name_kana],
-      birth_year: session[:birth_year], 
-      birth_month: session[:birth_month], 
-      birth_day: session[:birth_day], 
-      mobile_number: user_params[:mobile_number]
-    )
-      if @user.save
-        sign_in(@user)
-        redirect_to address_signup_index_path
-      else
-        render address_signup_index_path
-      end
+def create
+  @user = User.new(
+    nickname: session[:nickname],
+    email: session[:email],
+    password: session[:password],
+    password_confirmation: session[:password_confirmation],
+    last_name: session[:last_name], 
+    first_name: session[:first_name], 
+    last_name_kana: session[:last_name_kana], 
+    first_name_kana: session[:first_name_kana],
+    birth_year: session[:birth_year], 
+    birth_month: session[:birth_month], 
+    birth_day: session[:birth_day], 
+    mobile_number: user_params[:mobile_number]
+  )
+  if @user.save
+    sign_in(@user)
+    redirect_to address_signup_index_path
+  else
+    render address_signup_index_path
   end
+end
+def address
+end
 
-  def card  
-  end
+def card  
+end
 
-  def complete  
-  end
+def complete  
+end
 
 private
 
-  def user_params
-    params.require(:user).permit(
-      :nickname, 
-      :email, 
-      :password, 
-      :password_confirmation, 
-      :last_name, 
-      :first_name, 
-      :last_name_kana, 
-      :first_name_kana, 
-      :birth_year,
-      :birth_month,
-      :birth_day,
-      :mobile_number,
-      :sales_money,
-      :profile
-  )
-  end
- end 
+def user_params
+  params.require(:user).permit(
+    :nickname, 
+    :email, 
+    :password, 
+    :password_confirmation, 
+    :last_name, 
+    :first_name, 
+    :last_name_kana, 
+    :first_name_kana, 
+    :birth_year,
+    :birth_month,
+    :birth_day,
+    :mobile_number,
+    :sales_money,
+    :profile
+)
+end
+end
