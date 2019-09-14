f6debccc7809e0146f54b1e616cfddc625170131
Guchi-Y <i25mas25@gmail.com>
2019-09-14 14:39:43 +0900

    user_addressテーブルの本人情報を編集し上書きできる様にした

--- app/controllers/user_addresses_controller.rb
+++ app/controllers/user_addresses_controller.rb
@@ -1,4 +1,17 @@
 class UserAddressesController < ApplicationController
   def update
+    user_address = UserAddress.find_by(user_id: current_user.id)
+    user_address.update(user_address_params)
+
+    if user_address.save
+      redirect_to personal_info_user_mypage_path(user_id: params[:id])
+    else
+      render personal_info_user_mypage_path(user_id: params[:id])
+    end
+  end
+
+  private
+  def user_address_params
+    params.require(:user_address).permit(:postcode, :prefecture_id, :city, :block, :building)
   end
 end
