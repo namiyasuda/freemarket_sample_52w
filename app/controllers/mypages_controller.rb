26402d34120270a1ca4f180b41bc8cde84380e54
Guchi-Y <i25mas25@gmail.com>
2019-09-14 10:34:29 +0900

    マイページにログイン中のユーザー情報を表示する様に変更

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,5 +1,7 @@
 class MypagesController < ApplicationController
   def show
+    @user = User.find(current_user.id)
+    @products = Product.where('seller_id LIKE(?)', current_user.id)
   end
 
   def profile
