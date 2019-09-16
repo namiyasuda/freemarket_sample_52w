ab220da456b51be4a12087ddbb4da84bf95f03b1
savan7777 <yokoi0926@gmail.com>
2019-09-16 18:28:38 +0900

    DBより商品一覧の表示(簡易)

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,7 @@
 class TopsController < ApplicationController
   def index
-    @products = Product.order('id ASC').limit(4)
-    @images = Image.find(1)
+    # @products = Product.order('id ASC').limit(4)
+    @images = Image.order('id ASC').limit(4)
+   
   end
 end
