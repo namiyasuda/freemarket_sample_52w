c83e8dd08aef51e8277203b22c558b62de0b2191
savan7777 <yokoi0926@gmail.com>
2019-09-17 23:02:49 +0900

    商品一覧の表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,9 @@
 class TopsController < ApplicationController
   def index
-    # @products = Product.order('id ASC').limit(4)
-    @images = Image.order('id ASC').limit(4)
-   
+    @images1 = Image.limit(4)
+    @images2 = Image.where("id >= ? and id <= ?", 5, 8)
+    @images3 = Image.where("id >= ? and id <= ?", 9, 12)
+    @images4 = Image.where("id >= ? and id <= ?", 13, 16)
+    @images5 = Image.where("id >= ? and id <= ?", 17, 20)
   end
 end
