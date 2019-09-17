cbd616db000d290e864aa69d54241deb4ac0bba8
Guchi-Y <i25mas25@gmail.com>
2019-09-17 21:55:29 +0900

    LIKE(?)は不要の為削除した

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,8 +2,7 @@ class MypagesController < ApplicationController
   before_action :move_to_login
 
   def show
-    @user = User.find(current_user.id)
-    @products = Product.where('seller_id LIKE(?)', current_user.id)
+    @products = Product.where(seller_id: current_user.id)
   end
 
   def profile
