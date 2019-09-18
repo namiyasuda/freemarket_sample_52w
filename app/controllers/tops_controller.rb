0248a79073f704df687f32a661224da9a9f6e03b
savan7777 <yokoi0926@gmail.com>
2019-09-18 21:38:40 +0900

    最新の商品からトップに表示するよう修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,9 +1,9 @@
 class TopsController < ApplicationController
   def index
-    @images1 = Image.limit(4)
-    @images2 = Image.where("id >= ? and id <= ?", 5, 8)
-    @images3 = Image.where("id >= ? and id <= ?", 9, 12)
-    @images4 = Image.where("id >= ? and id <= ?", 13, 16)
-    @images5 = Image.where("id >= ? and id <= ?", 17, 20)
+    @images1 = Image.limit(4).order('id DESC')
+    @images2 = Image.limit(8).order('id DESC')-@images1
+    @images3 = Image.limit(12).order('id DESC')-@images1-@images2
+    @images4 = Image.limit(16).order('id DESC')-@images1-@images2-@images3
+    @images5 = Image.limit(20).order('id DESC')-@images1-@images2-@images3-@images4
   end
 end
