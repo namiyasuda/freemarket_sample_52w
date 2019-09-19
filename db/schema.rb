b6f966a419be0f004cfa9ee998a74c8e87b7e9b2
YusukeHoshin0 <h1133082@gmail.com>
2019-09-19 18:32:22 +0900

    マージ後の修正

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 2019_09_19_004833) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["name"], name: "index_brands_on_name"
   end
 
