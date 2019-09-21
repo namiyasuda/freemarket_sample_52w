76c9c3ddf9bc4c567107f7bb2066a1f7334d8556
Guchi-Y <i25mas25@gmail.com>
2019-09-21 16:58:54 +0900

    sns_credentialsテーブルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :seller_transactions, class_name: 'Product'
   has_many :buyer_transactions, class_name: 'Product'
-  # has_many :sns_credentials, dependent: :destroy
+  has_many :sns_credentials, dependent: :destroy
   # has_many :points 
   has_one :delivery_address, dependent: :destroy
   has_one :card, dependent: :destroy
-  # has_one :user_addresse, dependent: :destroy
+  has_one :user_addresse, dependent: :destroy
   belongs_to_active_hash :birth_year
+
+  devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_004833) do
+ActiveRecord::Schema.define(version: 2019_09_21_071648) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_brands_on_name"
   end
 
@@ -101,6 +101,15 @@ ActiveRecord::Schema.define(version: 2019_09_19_004833) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_sizes_on_ancestry"
+  end
+
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "uid"
+    t.string "provider"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "user_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
