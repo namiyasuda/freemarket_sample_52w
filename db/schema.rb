348a430e0f3d60d36b55a25bb651d5ac0eaa7b1d
Guchi-Y <i25mas25@gmail.com>
2019-09-22 15:29:11 +0900

    sns_credentialsテーブルのカラムにNOT NULL制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -105,8 +105,8 @@ ActiveRecord::Schema.define(version: 2019_09_21_071648) do
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.string "uid"
-    t.string "provider"
+    t.string "uid", null: false
+    t.string "provider", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_sns_credentials_on_user_id"
