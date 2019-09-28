f3154a3e374b1a3c5d7bdca8f87fb9291e176db1
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-09-28 15:46:32 +0900

    where first をfind byに修正

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -3,7 +3,7 @@ class BuysController < ApplicationController
     @product = Product.find_by(id: params[:product_id])
     @image = Image.find_by(id: params[:product_id])
     @delivery_addresses = DeliveryAddress.find_by(user_id: current_user.id)
-    @card = Card.where(user_id: current_user.id).first
+    @card = Card.find_by(user_id: current_user.id)
     if @card.present?
       Payjp.api_key = 'sk_test_d186521dfc37df79995c04e3'
       customer = Payjp::Customer.retrieve(@card.customer_id)
