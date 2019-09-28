d9bba553d2525891c41430586288b4305af496c8
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-09-28 15:21:39 +0900

    card.rbにメソッド定義（クレジットカードアイコンの取得）

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,29 +4,12 @@ class BuysController < ApplicationController
     @image = Image.find_by(id: params[:product_id])
     @delivery_addresses = DeliveryAddress.find_by(user_id: current_user.id)
     @card = Card.where(user_id: current_user.id).first
-    if @card.blank?
-      # redirect_to  product_buys_path(user_id: current_user.id)
-    else
+    if @card.present?
       Payjp.api_key = 'sk_test_d186521dfc37df79995c04e3'
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
       @card_brand = @default_card_information.brand
-      case @card_brand
-      when "Visa"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/visa.svg?575670517"
-      when "JCB"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/jcb.svg?575670517"
-      when "MasterCard"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/master-card.svg?575670517"
-      when "American Express"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/american_express.svg?575670517"
-      when "Diners Club"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/dinersclub.svg?575670517"
-      when "Discover"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/discover.svg?575670517"
-      when "SaisonCard"
-        @card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/saison-card.svg?575670517"
-      end
+      @card_src = Card.set_card_brand_icon(@card_brand)
     end
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,22 @@
 class Card < ApplicationRecord
   belongs_to :user
+  def self.set_card_brand_icon(card_brand)
+    case card_brand
+    when "Visa"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/visa.svg?575670517"
+    when "JCB"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/jcb.svg?575670517"
+    when "MasterCard"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/master-card.svg?575670517"
+    when "American Express"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/american_express.svg?575670517"
+    when "Diners Club"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/dinersclub.svg?575670517"
+    when "Discover"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/discover.svg?575670517"
+    when "SaisonCard"
+      card_src = "https://www-mercari-jp.akamaized.net/assets/img/card/saison-card.svg?575670517"
+    end
+    return card_src
+  end
 end
