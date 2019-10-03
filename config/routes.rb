36c4321079879ebaab53f9f5769c4ca3d213a6cb
savan7777 <yokoi0926@gmail.com>
2019-10-03 21:36:34 +0900

    検索結果ページのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'tops#index'
-  resources :tops, only: [:index]
+  resources :tops, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end  
 
   resources :delivery_addresses, only: [:new, :create, :update]
   resources :cards, only: [:new, :show,:create, :destroy]
