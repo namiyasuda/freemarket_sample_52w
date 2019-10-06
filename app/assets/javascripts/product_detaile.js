abcbf763e059ac6dc5d998b6f407acc39f5b6197
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-10-06 09:19:29 +0900

    画像切替表示の実装

---
+++ app/assets/javascripts/product_detaile.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('img.details__photo__under__image').click(function() {
+    var ImgSrc = $(this).attr("src");
+    $("img#mainphoto").attr({src:ImgSrc});
+    $("img#mainphoto").hide();
+    $("img#mainphoto").fadeIn();
+        return false;
+    });
+  });
+;
