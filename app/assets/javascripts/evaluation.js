1ff1ca42ca39db2d24af46e4c4a0a4874d24b5fb
YusukeHoshin0 <h1133082@gmail.com>
2019-10-09 19:33:39 +0900

    sider修正

--- app/assets/javascripts/evaluation.js
+++ app/assets/javascripts/evaluation.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.evaluation__body__form__submit').on('click', function (){
-    if ($('.evaluation-radio-btn').prop("checked" )== false) {
+    if (!$('input:radio[name="product[evaluation]"]:checked').val()) {
       $('.evaluation__body__form__alert').removeClass('hide');
     }
   })
