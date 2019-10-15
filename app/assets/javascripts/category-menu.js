81ee8dbf617464b40f322ecb8267895ec3f695cb
namiyasuda <51950024+namiyasuda@users.noreply.github.com>
2019-10-15 16:50:16 +0900

    子、孫カテゴリーのプルダウンメニューの実装/.jsに記述

--- app/assets/javascripts/category-menu.js
+++ app/assets/javascripts/category-menu.js
@@ -1,40 +1,72 @@
 $(function() {
+  let left_index;
+  let center_index;
+  let center_children_index;
+  let index;
+  // 親要素を表示する
   $('.category-search').hover(function() {
-    $('.category-meun__left').removeClass('active');
-  },function() {
+    $('.category-meun__left').removeClass('hide');
+    $('.category-meun__center').addClass('hide');
+    $('.category-meun__right').addClass('hide');
+  },function(){
   });
-  $('.category-meun__left').hover(function() {
+  // 親要素を非表示にする 
+  $('.category-meun').hover(function() {
   },function() {
-  $('.category-meun__left').addClass('active');
+    $('.category-meun__left').addClass('hide');
+    $('.category-meun__center').addClass('hide');
+    $('.category-meun__right').addClass('hide');
+  });
+  // 子要素を表示する
+  $('.category-meun__left__parent').hover(function() {
+    left_index = $(this).index();
+    $('.category-meun__center').removeClass('hide');
+    $('.category-meun__center__parent').eq(left_index).removeClass('hide');
+    $('.category-meun__right').addClass('hide');
+  },function() {
+    $('.category-meun__center__parent').eq(left_index).addClass('hide');
+  });
+  
+  $('.category-meun__center__children').hover(function() {
+    center_index = $(this).parent().index();
+    $('.category-meun__center__parent').eq(center_index).removeClass('hide');
+    $('.category-meun__right').removeClass('hide');
+    $('.category-meun__center').removeClass('hide');
+    center_children_index = $(this).index();
+    $('.category-meun__right__parent').eq(center_index).children().eq(center_children_index).removeClass('hide');
+  },function() {
+    $('.category-meun__right__parent').eq(center_index).children().eq(center_children_index).addClass('hide');
+    $('.category-meun__right').addClass('hide');
+  });
+
+  $('.category-meun__center__parent').hover(function() {
+    index = $(this).index();
+    $('.category-meun__left__parent').eq(index).css({'background-color':'#ea352d','color':'#fff'});
+    $('.category-meun__center__parent').eq(index).removeClass('hide');
+    $('.category-meun__center').removeClass('hide');
+  },function() {
+    $('.category-meun__center__parent').eq(index).addClass('hide');
+    $('.category-meun__left__parent').eq(index).css({'background-color':'','color':''});
+  });
+
+  $('.category-meun__right__parent').hover(function() {
+    $('.category-meun__left__parent').eq(index).css({'background-color':'#ea352d','color':'#fff'});
+    $('.category-meun__center__parent').eq(center_index).removeClass('hide');
+    $('.category-meun__right').removeClass('hide');
+    $('.category-meun__center').removeClass('hide');
+    $('.category-meun__right__parent').eq(center_index).children().eq(center_children_index).removeClass('hide');
+  },function() {
+    $('.category-meun__right__parent').eq(center_index).children().eq(center_children_index).addClass('hide');
+    $('.category-meun__left__parent').eq(index).css({'background-color':'','color':''});
+  });
+
+  $('.category-meun__right__grandchildren').hover(function() {
+    $('.category-meun__center__parent').eq(center_index).removeClass('hide');
+    $('.category-meun__center__parent').eq(center_index).children().eq(center_children_index).css('background-color','#eee');
+    $('.category-meun__right').removeClass('hide');
+    $('.category-meun__center').removeClass('hide');
+    $('.category-meun__right__parent').eq(center_index).children().eq(center_children_index).removeClass('hide');
+  },function() {
+    $('.category-meun__center__parent').eq(center_index).children().eq(center_children_index).css('background-color','');
   });
 });
-
-  // 親カテゴリー選択で子カテゴリーを生成
-  $('.category-meun__left__parent').hover(function () {
-    var parent_id = $('.category-meun__left__parent option:selected').val();
-    if (parent_id.length != 0){
-      $('#category-child-1').removeClass('hide');
-      disappearSizeBox();
-      disapperGrandchild();
-      $('#product-brand').removeClass('hide');
-      $.ajax({
-        url: '/tops/downlist_children',
-        type: 'Get',
-        data: {parent_id: parent_id},
-        dataType: 'json'
-      }).done(function(children){
-        $('#category-child-1 option').remove();
-        $('#category-child-2 option').remove();
-        var insertHTML = `<option value="">---</option>`;
-        children.forEach(function(child){
-          insertHTML += appendOption(child);
-        })
-        $('#child_category').append(insertHTML);
-      }).fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      })
-    } else {
-      $('#category-child-1').addClass('hide');
-      disapperGrandchild();
-    }
-  });
