b77dfd7909f072e3f1f2a018c18db20b3fd21410
YusukeHoshin0 <h1133082@gmail.com>
2019-10-20 19:06:16 +0900

    ルーティングの順序を変更k

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,23 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users, :controllers => {
     omniauth_callbacks:  "users/omniauth_callbacks",
     sessions: "users/sessions"
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do   
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   
   root 'tops#index'
+ 
+  # トップページ
   resources :tops, only: [:index] do
     collection do
       get 'search'
     end
   end
 
-  resources :delivery_addresses, only: [:new, :create, :update]
-  resources :cards, only: [:new, :show,:create, :destroy]
-  resources :signup, only: [:new, :create] do
-    collection do 
-      get 'customer_info'
-      get 'sms_comfi'
-      get 'complete'
-    end
-  end
-
-  resources :products, only: [:show, :new, :create, :edit, :update, :destroy] do
-    # Ajaxで動作するルーティングを設定
-    collection do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-      get 'get_size', defaults: { format: 'json' }
-      get 'get_delivery_method', defaults: { format: 'json' }
-      get 'dropzone'
-      patch ':id/stop_listing' => 'products#stop_listing'
-      patch ':id/restart_listing' => 'products#restart_listing'
-    end
-
-    member do
-      get 'evaluation'
-      patch 'evaluate'
-    end
-    
-    resource :buys, only: [:show] do
-      collection do
-        patch 'pay'
-      end
-    end
-  end  
-  
+  # ユーザー関連
   resources :users, only: [:update] do
     resource :mypage, only: [:show] do
       collection do 
@@ -69,16 +40,46 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :user_addresses, only: [:update]
-
-  devise_scope :user do   
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-  
   resources :auth_signup, only: [:create] do
     collection do 
       get 'auth_sms_comfi'
     end
   end
+  
+  resources :signup, only: [:new, :create] do
+    collection do 
+      get 'customer_info'
+      get 'sms_comfi'
+      get 'complete'
+    end
+  end
+
+  resources :user_addresses, only: [:update]
+  resources :delivery_addresses, only: [:new, :create, :update]
+  resources :cards, only: [:new, :show,:create, :destroy]
+
+
+  # 商品関連
+  resources :products, only: [:show, :new, :create, :edit, :update, :destroy] do
+    # Ajaxで動作するルーティングを設定
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+      get 'get_size', defaults: { format: 'json' }
+      get 'get_delivery_method', defaults: { format: 'json' }
+      get 'dropzone'
+      patch ':id/stop_listing' => 'products#stop_listing'
+      patch ':id/restart_listing' => 'products#restart_listing'
+    end
+    member do
+      get 'evaluation'
+      patch 'evaluate'
+    end
+    resource :buys, only: [:show] do
+      collection do
+        patch 'pay'
+      end
+    end
+  end  
 
 end
